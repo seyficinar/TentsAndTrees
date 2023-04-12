7662b78c1da0243ce257a7639bf681ac0aa58690
xyz <xyz@DESKTOP-QMV1UFK.local>
2023-04-12 19:14:13 +0300

    I pushed these before but I guess it was not proper repository

---
+++ TentAndTree/src/Piece.java
@@ -0,0 +1,51 @@
+
+//Class for identifying pieces in the puzzle which could be Tree(T), Tent(X) and O is neither tent nor tree given in the input
+
+public class Piece {
+
+	char type; // (T, X or O)
+	Piece next; // to access other pieces
+
+	// Coordinate values of pieces
+	int cordX;
+	int cordY;
+	
+	//Constructor
+	public Piece(char type, Piece next, int cordX, int cordY) {
+
+	}
+
+	// Getters and Setters
+	public char getType() {
+
+	}
+
+	public void setType(char type) {
+
+	}
+
+	public Piece getNext() {
+
+	}
+
+	public void setNext(Piece next) {
+
+	}
+
+	public int getCordX() {
+
+	}
+
+	public void setCordX(int cordX) {
+
+	}
+
+	public int getCordY() {
+
+	}
+
+	public void setCordY(int cordY) {
+
+	}
+
+}
---
+++ TentAndTree/src/Queue.java
@@ -0,0 +1,33 @@
+
+public class Queue {
+	
+	Piece first;
+	Piece last;
+	
+	
+	
+	
+	
+	
+	
+	//Constructor
+    public PieceQueue() {
+        
+    }
+    
+    //Returns false if queue is not empty, otherwise if it is empty
+    public boolean isEmpty(){
+        
+    }
+    
+    //New piece added to queue
+    public void enqueue(Piece newPiece) {
+        
+    }
+    
+    //Deletes the puzzle from queue
+    public Piece dequeue(){
+        
+    }
+
+}
