1794fc7b7fea582287dabc64e70adadfe5851cf7
xyz <xyz@DESKTOP-QMV1UFK.local>
2023-04-15 22:56:44 +0300

    Deleting previous javadocs

--- TentAndTree/src/Main.java
+++ TentAndTree/src/Main.java
@@ -4,15 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 
 
-		//Get the input with scanner
 		
-		
-		//Creating the queue, and placing the pieces of puzzle into the queue
-		
-		
-		//Calling possibleTents()
-		
-		//Calling minimizedTents()
 		
 		
 		
